1476431b3892da93b097fba4599f993e09f534d7
AoyuQC <aoyuzhang1989@gmail.com>
2024-05-27 13:54:08 +0000

    add simple inference

---
+++ simple_inference.sh
@@ -0,0 +1,8 @@
+/home/ubuntu/pytorch_gpu_base_ubuntu_uw2_workplace/anaconda3/envs/vto/bin/accelerate launch inference.py \
+    --width 768 --height 1024 --num_inference_steps 30 \
+    --output_dir "result" \
+    --unpaired \
+    --data_dir "DATA_DIR" \
+    --seed 42 \
+    --test_batch_size 2 \
+    --guidance_scale 2.0
